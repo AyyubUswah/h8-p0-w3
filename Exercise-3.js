21eed18a0dd3250f581e5805457c626705bdd309
Ayyub Uswah <uswah1997@gmail.com>
2019-12-02 11:53:14 +0700

    revisi tugas 3

--- Exercise-3.js
+++ Exercise-3.js
@@ -1,5 +1,3 @@
-
-
 var input = [
     ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
     ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
@@ -8,25 +6,25 @@ var input = [
 ]
 
 function dataHandling (array) {
-    if (array === input[0]) {
+    if (array === input[0][0]) {
     console.log('Nomor ID: ' + input[0][1]);
     console.log('Nama Lengkap: ' + input[0][2]);
     console.log('TTL: ' + input[0][3]);
     console.log('Hobi: ' + input[0][4]);
     }
-    else if (array === input[1]) {
+    else if (array === input[1][0]) {
     console.log('Nomor ID: ' + input[1][1]);
     console.log('Nama Lengkap: ' + input[1][2]);
     console.log('TTL: ' + input[1][3]);
     console.log('Hobi: ' + input[1][4]);
     }
-    else if (array === input[2]) {
+    else if (array === input[2][0]) {
     console.log('Nomor ID: ' + input[2][1]);
     console.log('Nama Lengkap: ' + input[2][2]);
     console.log('TTL: ' + input[2][3]);
     console.log('Hobi: ' + input[2][4]);
     }
-    else if ( array === input[3]) {
+    else if ( array === input[3][0]) {
     console.log('Nomor ID: ' + input[3][1]);
     console.log('Nama Lengkap: ' + input[3][2]);
     console.log('TTL: ' + input[3][3]);
@@ -34,10 +32,10 @@ function dataHandling (array) {
     }
 }
 
-dataHandling (input[0]);
+dataHandling ("0001");
 console.log('');
-dataHandling (input[1]);
+dataHandling ("0002");
 console.log('');
-dataHandling (input[2]);
+dataHandling ("0003");
 console.log('');
-dataHandling (input[3]);
+dataHandling ("0004");
